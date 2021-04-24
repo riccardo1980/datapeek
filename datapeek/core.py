5c43d94c905858a42f6d4352f82deae6151de03c
Riccardo Zanella <riccardo.zanella@gmail.com>
2021-04-24 12:17:39 +0200

    adding basic files for project configiuration

---
+++ datapeek/core.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+
+def datapeek_core(input: str, output: str):
+    """
+    Calls the profiling tool
+    """
+    pass
---
+++ main.py
@@ -0,0 +1,14 @@
+
+from datapeek.core import datapeek_core
+
+
+def datapeek(event, context):
+    pass
+
+
+if __name__ == "__main__":
+    infile = ''
+    outfile = ''
+
+    datapeek_core(infile, outfile)    
+     
